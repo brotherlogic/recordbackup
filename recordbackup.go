3d1963c8d00eae5a8cbd6c09f9eccac31bbf399a
Simon Tucker <simon@brotherlogic@gmail>
2019-04-26 21:44:39 -0700

    Adds counter

--- recordbackup.go
+++ recordbackup.go
@@ -77,7 +77,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "stored", Value: int64(len(s.config.Metadata))},
+	}
 }
 
 func main() {
