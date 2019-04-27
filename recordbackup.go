733fa758d49c66c0318a39c249cf53673e881509
Simon Tucker <simon@brotherlogic@gmail>
2019-04-26 21:35:41 -0700

    Adds files

---
+++ recordbackup.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"github.com/brotherlogic/goserver"
+	"github.com/brotherlogic/keystore/client"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pbg "github.com/brotherlogic/goserver/proto"
+	pb "github.com/brotherlogic/recordbackup/proto"
+)
+
+const (
+	// KEY - where records are backed up
+	KEY = "/github.com/brotherlogic/recordbackup/config"
+)
+
+//Server main server type
+type Server struct {
+	*goserver.GoServer
+	config *pb.Config
+}
+
+// Init builds the server
+func Init() *Server {
+	s := &Server{
+		&goserver.GoServer{},
+		&pb.Config{},
+	}
+	return s
+}
+
+func (s *Server) save(ctx context.Context) {
+	s.KSclient.Save(ctx, KEY, s.config)
+}
+
+func (s *Server) load(ctx context.Context) error {
+	config := &pb.Config{}
+	data, _, err := s.KSclient.Read(ctx, KEY, config)
+
+	if err != nil {
+		return err
+	}
+
+	s.config = data.(*pb.Config)
+	return nil
+}
+
+// DoRegister does RPC registration
+func (s *Server) DoRegister(server *grpc.Server) {
+	// Pass
+}
+
+// ReportHealth alerts if we're not healthy
+func (s *Server) ReportHealth() bool {
+	return true
+}
+
+// Shutdown the server
+func (s *Server) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+// Mote promotes/demotes this server
+func (s *Server) Mote(ctx context.Context, master bool) error {
+	if master {
+		err := s.load(ctx)
+		return err
+	}
+
+	return nil
+}
+
+// GetState gets the state of the server
+func (s *Server) GetState() []*pbg.State {
+	return []*pbg.State{}
+}
+
+func main() {
+	var quiet = flag.Bool("quiet", false, "Show all output")
+	flag.Parse()
+
+	//Turn off logging
+	if *quiet {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+	server := Init()
+	server.GoServer.KSclient = *keystoreclient.GetClient(server.GetIP)
+	server.PrepServer()
+	server.Register = server
+
+	err := server.RegisterServer("recordbackup", false)
+	if err != nil {
+		log.Fatalf("Unable to register: %v", err)
+	}
+	server.Serve()
+}
